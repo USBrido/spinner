ebd6c9ada0b84ada13887fe9c796ee10e99fe875
USBrido <obrido@gmail.com>
2019-12-03 19:55:57 +0000

    Spinner.js

--- spinner2.js
+++ spinner2.js
@@ -1,24 +1,17 @@
 // process.stdout.write('hello from spinner1.js... \rheyyy\n');
-
-
-
 // setTimeout(() => {
 //   process.stdout.write('\r|   ');
 // }, 100);
-
 // setTimeout(() => {
 //   process.stdout.write('\r/   ');
 // }, 300);
-
 // setTimeout(() => {
 //   process.stdout.write('\r-   ');
 // }, 500);
-
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
 //   process.stdout.write('\r\\   '); 
 // }, 700);
-
 // setTimeout(() => {
 //   // Need to escape the backslash since it's a special character.
 //   process.stdout.write('\r|   '); 
@@ -27,11 +20,18 @@
 
 // //use the stop process from lecture to loop out once set time is done
 
-const sentence = ['|', '/', '-', '\\', '|', '\n'];
+let argv =  process.argv.slice(2);
+argv = argv.sort((a, b) => a -b);
 let timer = 0;
-for (const char of sentence) {
-  timer += 200;
- setTimeout(() => {
-  process.stdout.write (`\r${char}`)
+for (const time of argv) {
+  timer =  time * 1000;
+  if ( argv === []) {
+    return console.log(`No alarm!`);
+  } else if (time < 0 || time == NaN) {
+    timer++
+  } else {
+  setTimeout(() => {
+  process.stdout.write ('\x07')
   }, timer);
+}
 };
